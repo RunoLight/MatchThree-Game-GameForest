4375d3e539dec4bd44dc6589656c2b738a531d33
Runo <31756050+RunoLight@users.noreply.github.com>
2020-06-19 13:15:13 +1000

    add game base samples

--- MatchThreeGameForest/Game1.cs
+++ MatchThreeGameForest/Game1.cs
@@ -1,4 +1,4 @@
-﻿using MatchThreeGameForest.Textures;
+﻿using MatchThreeGameForest.TextureRenderer;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
@@ -58,13 +58,11 @@ namespace MatchThreeGameForest
 
             // TODO: Add your drawing code here
 
-            spriteBatch.DrawString(font, "Score", new Vector2(100, 100), Color.Black);
+            spriteBatch.DrawString(font, "text sample", new Vector2(100, 100), Color.Black);
             spriteBatch.Draw(TextureManager.Cell, new Rectangle(30, 30, 50, 50), Color.White);
             spriteBatch.Draw(TextureManager.Diamond, new Rectangle(30, 30, 50, 50), Color.White);
 
             spriteBatch.End();
-
-
 
             base.Draw(gameTime);
         }
--- MatchThreeGameForest/SpriteRenderer/TextureManager.cs
+++ MatchThreeGameForest/SpriteRenderer/TextureManager.cs
@@ -2,7 +2,7 @@
 using Microsoft.Xna.Framework.Graphics;
 using System.Collections.Generic;
 
-namespace MatchThreeGameForest.Textures
+namespace MatchThreeGameForest.TextureRenderer
 {
     static class TextureManager
     {
