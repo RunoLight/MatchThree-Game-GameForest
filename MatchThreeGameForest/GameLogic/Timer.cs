9bcf00a142590cb4192c5785577639965db7ba68
Runo <31756050+RunoLight@users.noreply.github.com>
2020-06-20 19:27:33 +1000

    Timer

---
+++ MatchThreeGameForest/GameLogic/Timer.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+
+namespace MatchThreeGameForest.GameLogic
+{
+    static class Timer
+    {
+        private static double timeToWait;
+        private static bool isExpired;
+        private static Action callback;
+
+        public static void Reset(float newTime = 60)
+        {
+            timeToWait = newTime;
+            isExpired = false;
+            //TODO reset the callbacks?
+        }
+
+        public static void AddListener(Action listener)
+        {
+            callback += listener;
+        }
+
+        public static void Tick(GameTime gameTime)
+        {
+            if (!isExpired)
+            {
+                timeToWait -= gameTime.TotalGameTime.TotalSeconds;
+            }
+
+            if (timeToWait > 0)
+                return;
+
+            callback?.Invoke();
+        }
+
+    }
+}
